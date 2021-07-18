7527332910512462f720bffab78ce03f73cfb4a7
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-07-18 15:51:48 +0300

    Generate HTML reports - wip.

--- brick.py
+++ brick.py
@@ -7,6 +7,7 @@ import glob
 from logger import log_step, log_operation, log_timing, log_warning
 from modules import AVAILABLE_MODULE_NAMES, MODULE_DESCRIPTIONS
 from harvest.utils import harvest
+from yattag import Doc
 
 def compact(outdir, outfile, clean=False):
     # Lists of SMM executables that:
@@ -16,12 +17,12 @@ def compact(outdir, outfile, clean=False):
     #         further manual analysis is required to further classify it as either true positive or false positive.
     passed, ignore, verify = [], [], []
 
-    for module in (pathlib.Path(module) for module in glob.glob(f'{outdir}\\*.efi')):
+    for module in (pathlib.Path(module) for module in glob.glob(f'{outdir}\\*.i64')):
+        module = module.resolve()
         report = module.with_suffix('.brick')
         if os.path.exists(report):
             # At least one module issued a warning.
             data = open(report, 'r').read()
-            basename = module.stem.lower()
             # Unknown module (probably OEM-specific), might be a true positive.
             verify.append((str(module), data))
                 
@@ -30,42 +31,60 @@ def compact(outdir, outfile, clean=False):
             # No warnings.
             passed.append(str(module))
         
-    with open(outfile, 'w') as bf:
-        bf.write('*' * 50 + ' SUMMARY ' + '*' * 50)
-        bf.write('\n')
+    COLORS = {
+        'ERROR': 'red',
+        'WARNING': 'orange',
+        'INFO': 'green'
+    }
+
+    doc, tag, text = Doc().tagtext()
+    with tag('html'):
+        with tag('body'):
+            with tag('h1'):
+                text(f'SUMMARY OF SCAN RESULTS')
 
         for x in verify:
-            bf.write('''
-[*] --------------------------------------------------
-[*] {}
-[*] --------------------------------------------------'''.format(x[0]))
-            bf.write('\n')
-            bf.write(x[1])
+            i64 = x[0].replace('.efi', '.i64')
+            with tag('a', href=i64):
+                text(i64)
+            with tag('ul'):
+                for line in x[1].splitlines():
+                    level = line.split()[0]
+                    color = COLORS.get(level, 'black')
+                    # with tag('p', )
+                    with tag('li', style=f'color:{color}'):
+                        text(line)
+                # mdFile.write('<span style="color:blue">some *blue* text</span>')
+                # mdFile.new_line(line)
+            # bf.write(x[1])
+        # print(mdFile.file_data_text)
+    # html = markdown.markdown(mdFile.file_data_text)
+    open('brick.html', 'w').write(doc.getvalue())
 
 def main(rom, outdir, modules, verbose=False):
-    with log_step('Building GUIDs database'):
-        db = GuidsDatabase()
+    # with log_step('Building GUIDs database'):
+    #     db = GuidsDatabase()
     
-    with log_step('Harvesting SMM modules'):
-        harvest(rom, outdir, db.guid2name)
+    # with log_step('Harvesting SMM modules'):
+    #     harvest(rom, outdir, db.guid2name)
 
-    # # 64-bit binaries with .efi extension
-    hunter = Hunter(outdir, 64, '.efi', verbose)
+    # # # 64-bit binaries with .efi extension
+    # hunter = Hunter(outdir, 64, '.efi', verbose)
 
-    with log_step('Analyzing SMM modules'):
-        hunter.analyze()
+    # with log_step('Analyzing SMM modules'):
+    #     hunter.analyze()
 
-    with log_step('Cleaning-up temporary files'):
-        hunter.cleanup()
+    # with log_step('Cleaning-up temporary files'):
+    #     hunter.cleanup()
 
-    if modules is None:
-        modules = AVAILABLE_MODULE_NAMES
+    # if modules is None:
+    #     modules = AVAILABLE_MODULE_NAMES
 
-    bootstrap_script = pathlib.Path(__file__).parent / 'bootstrap.py'
+    # bootstrap_script = pathlib.Path(__file__).parent / 'bootstrap.py'
 
-    for mod in modules:
-        with log_step(MODULE_DESCRIPTIONS[mod]):
-            hunter.run_script(bootstrap_script, mod)
+    # for mod in modules:
+    #     with log_step(MODULE_DESCRIPTIONS[mod]):
+    #         hunter.run_script(bootstrap_script, mod)
 
     # Merge all individual output files into one file.
     with log_step('Compacting output files'):
