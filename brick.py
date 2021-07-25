80192f41c06c192270162e4fadd0e5af4a1a4907
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-07-26 01:45:05 +0300

    Move formatting code to its own dedicated package.

--- brick.py
+++ brick.py
@@ -1,104 +1,41 @@
-import os
 import pathlib
 import argparse
 from guids.guids_db import GuidsDatabase
 from hunter import Hunter
-import glob
 from logger import log_step, log_operation, log_timing, log_warning
-from modules import BRICK_MODULES_NAMES, BRICK_ODULES_DESCRIPTIONS
+from modules import BRICK_MODULES_NAMES, BRICK_MODULES_DESCRIPTIONS
 from harvest.utils import harvest
-from yattag import Doc
-
-def compact(outdir, outfile, clean=False):
-    results = []
-
-    for module in (pathlib.Path(module).resolve() for module in glob.glob(f'{outdir}\\*.efi')):
-        try:
-            report = module.with_suffix('.brick')
-            data = open(report, 'r').read()
-            results.append((str(module), data))
-                
-            if clean: os.remove(report)
-        except Exception as e:
-            log_warning(f'Failed processing {module}, exception {e}')
-            continue
-        
-    COLORS = {
-        'ERROR': 'red',
-        'WARNING': 'orange',
-        'SUCCESS': 'green',
-        'INFO': 'purple'
-    }
-
-    doc, tag, text = Doc().tagtext()
-    with tag('html'):
-        with tag('body'):
-            with tag('h1'):
-                text(f'SUMMARY OF SCAN RESULTS')
-
-        for x in results:
-            with tag('table', border='2px solid black', style='width:100%'):
-                with tag('tr'):
-                    with tag('th', colspan='3'):
-                        text(x[0])
-                with tag('tr'):
-                    with tag('td', colspan='3'):
-                        with tag('ul'):
-                            for line in x[1].splitlines():
-                                level = line.split()[0]
-                                color = COLORS.get(level, 'black')
-                                with tag('li', style=f'color:{color}'):
-                                    text(line)
-                with tag('tr'):
-                    with tag('td'):
-                        text('IDA database: ')
-                        idb = x[0].replace('.efi', '.i64')
-                        with tag('a', href=idb):
-                            text(idb)
-                    with tag('td'):
-                        text('IDA log: ')
-                        log = x[0].replace('.efi', '.log')
-                        with tag('a', href=log):
-                            text(log)
-                    with tag('td'):
-                        text('efiXplorer log: ')
-                        json = x[0].replace('.efi', '.json')
-                        with tag('a', href=json):
-                            text(json)
-            with tag('br'):
-                pass
-
-    open(outfile, 'w').write(doc.getvalue())
+from formatter.html import format
 
 def analyze(rom, outdir, modules, verbose=False):
-    with log_step('Building GUIDs database'):
-        db = GuidsDatabase()
+    # with log_step('Building GUIDs database'):
+    #     db = GuidsDatabase()
     
-    with log_step('Harvesting SMM modules'):
-        harvest(rom, outdir, db.guid2name)
+    # with log_step('Harvesting SMM modules'):
+    #     harvest(rom, outdir, db.guid2name)
 
-    # 64-bit binaries with .efi extension
-    hunter = Hunter(outdir, 64, '.efi', verbose)
+    # # 64-bit binaries with .efi extension
+    # hunter = Hunter(outdir, 64, '.efi', verbose)
 
-    with log_step('Analyzing SMM modules'):
-        hunter.analyze()
+    # with log_step('Analyzing SMM modules'):
+    #     hunter.analyze()
 
-    with log_step('Cleaning-up temporary files'):
-        hunter.cleanup()
+    # with log_step('Cleaning-up temporary files'):
+    #     hunter.cleanup()
 
-    if modules is None:
-        modules = BRICK_MODULES_NAMES
+    # if modules is None:
+    #     modules = BRICK_MODULES_NAMES
 
-    bootstrap_script = pathlib.Path(__file__).parent / 'bootstrap.py'
+    # bootstrap_script = pathlib.Path(__file__).parent / 'bootstrap.py'
 
-    for mod in modules:
-        with log_step(BRICK_ODULES_DESCRIPTIONS[mod]):
-            hunter.run_script(bootstrap_script, mod)
+    # for mod in modules:
+    #     with log_step(BRICK_MODULES_DESCRIPTIONS[mod]):
+    #         hunter.run_script(bootstrap_script, mod)
 
     # Merge all individual output files into one report file, formatted as HTML.
     html_report = f'{rom}.html'
-    with log_step('Compacting output files'):
-        compact(outdir, html_report)
+    with log_step('Formatting output files'):
+        format(outdir, html_report)
 
     return html_report
 
---
+++ formatter/html.py
@@ -0,0 +1,76 @@
+from pathlib import Path
+import glob
+import os
+from yattag import Doc
+from logger import log_warning
+
+def format(outdir, outfile, clean=False):
+    results = []
+
+    for module in (Path(module).resolve() for module in glob.glob(f'{outdir}\\*.efi')):
+        try:
+            report = module.with_suffix('.brick')
+            data = open(report, 'r').read()
+            results.append((str(module), data))
+                
+            if clean: os.remove(report)
+        except Exception as e:
+            log_warning(f'Failed processing {module}, exception {e}')
+            continue
+        
+    COLORS = {
+        'ERROR': 'red',
+        'WARNING': 'orange',
+        'SUCCESS': 'green',
+        'INFO': 'purple'
+    }
+
+    doc, tag, text = Doc().tagtext()
+    with tag('html'):
+        with tag('script'):
+            checkbox_script = Path(__file__).parent / 'checkbox.js'
+            doc.asis(open(checkbox_script, 'r').read())
+
+        with tag('body', onload='javascript:init()'):
+            with tag('h1'):
+                text(f'SUMMARY OF SCAN RESULTS')
+
+        id = 0
+        for x in results:
+            with tag('table', border='2px solid black', style='width:100%'):
+                with tag('tr'):
+                    with tag('th', colspan='3'):
+                        text(x[0])
+                with tag('tr'):
+                    with tag('td', colspan='3'):
+                        with tag('ul'):
+                            for line in x[1].splitlines():
+                                id += 1
+                                level = line.split()[0]
+                                color = COLORS.get(level, 'black')
+                                if level == 'ERROR':
+                                    with tag('input', type='checkbox', id=f'checkbox_{id}', onchange=f'javascript:do_strikethrough(this, {id})'):
+                                        pass
+                                with tag('li', id=f'li_{id}', style=f'color:{color}'):
+                                    text(line)
+                            
+                with tag('tr'):
+                    with tag('td'):
+                        text('IDA database: ')
+                        idb = x[0].replace('.efi', '.i64')
+                        with tag('a', href=idb):
+                            text(idb)
+                    with tag('td'):
+                        text('Raw IDA log: ')
+                        log = x[0].replace('.efi', '.log')
+                        with tag('a', href=log):
+                            text(log)
+                    with tag('td'):
+                        text('efiXplorer report: ')
+                        json = x[0].replace('.efi', '.json')
+                        with tag('a', href=json):
+                            text(json)
+            with tag('br'):
+                pass
+
+    open(outfile, 'w').write(doc.getvalue())
