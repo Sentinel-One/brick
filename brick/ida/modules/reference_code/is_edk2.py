fe21576950a051238226ff82b957248a8bbb862f
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-08-31 22:54:07 +0300

    fix reference to EDK2 external.

--- brick/ida/modules/reference_code/is_edk2.py
+++ brick/ida/modules/reference_code/is_edk2.py
@@ -6,15 +6,18 @@ from edk2toollib.uefi.edk2.guid_list import GuidList
 from bip.base import *
 from ..base_module import BaseModule
 
+from externals import get_external
+
 class IsEdk2Module(BaseModule):
+
+    EDK2_DIR = str(get_external('edk2'))
 
     def __init__(self) -> None:
         super().__init__()
 
     def run(self):
-        edk2_dir = Path(__file__).parent / 'edk2'
-        edk2_names = [n.name.lower() for n in GuidList.guidlist_from_filesystem(edk2_dir)]
-        edk2_guids = [g.guid.lower() for g in GuidList.guidlist_from_filesystem(edk2_dir)]
+        edk2_names = [n.name.lower() for n in GuidList.guidlist_from_filesystem(self.EDK2_DIR)]
+        edk2_guids = [g.guid.lower() for g in GuidList.guidlist_from_filesystem(self.EDK2_DIR)]
 
         basename = Path(idc.get_input_file_path()).stem
         if basename.lower() in edk2_guids or basename.lower() in edk2_names:
