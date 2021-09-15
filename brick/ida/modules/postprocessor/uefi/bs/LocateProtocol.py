a9a0881d03aa5f746ff1fab964fd1ac5c1fdd2f7
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-09-16 01:11:31 +0300

    Fix renaming and typing of protocol interfaces to match the new naming convention used by efiXplorer.

--- brick/ida/modules/postprocessor/uefi/bs/LocateProtocol.py
+++ brick/ida/modules/postprocessor/uefi/bs/LocateProtocol.py
@@ -2,6 +2,13 @@ from ..base import CNodeExprIndirectCall, set_cnode_name, set_cnode_type
 
 from bip.base import *
 from bip.hexrays import *
+import re
+
+
+def camel_to_snake(name):
+    name = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
+
 
 class LocateProtocolCall(CNodeExprIndirectCall):
     """Represents a call to LocateProtocol()"""
@@ -27,21 +34,30 @@ class LocateProtocolCall(CNodeExprIndirectCall):
             protocol = protocol.ops[0].ignore_cast
 
         # We expect the protocol GUID to be a global variable.
-        if not isinstance(protocol, CNodeExprObj):
+        if isinstance(protocol, CNodeExprObj):
+            protocol_name = BipElt(protocol.value).name
+        elif isinstance(protocol, CNodeExprVar):
+            protocol_name = protocol.lvar.name
+        else:
             return
 
         # Get a friendly name for the protocol.
         # For example, use just EFI_SMM_VARIABLE_PROTOCOL instead of EFI_SMM_VARIABLE_PROTOCOL_GUID_8A40.
-        protocol_name = BipElt(protocol.value).name
-        protocol_name = protocol_name[:protocol_name.find('_GUID_')]
+        if (idx := (protocol_name.find('_GUID_'))) != -1:
+            interface_name = protocol_name[:idx]
+        elif protocol_name.startswith('gEfi') and (idx := protocol_name.find('Guid_')) != -1:
+            interface_name = camel_to_snake(protocol_name[1:idx]).upper()
+            print(f'Interface name is {interface_name}')
+        else:
+            interface_name = protocol_name
 
         interface = self.Interface.ignore_cast
 
         # Rename the interface pointer to match the protocol's name.
-        set_cnode_name(interface, protocol_name)
+        set_cnode_name(interface, interface_name)
 
         try:
-            interface_pointer_type = BipType.from_c(f'{protocol_name} *')
+            interface_pointer_type = BipType.from_c(f'{interface_name} *')
         except RuntimeError:
             # The pointer type is not recognized, set to PVOID.
             interface_pointer_type = BipType.from_c('void *')
