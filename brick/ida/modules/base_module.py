6aca0c19b59b4d879de510c51e348cb409a98f18
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-09-16 22:19:35 +0300

    Cache some useful properties in the base module.

--- brick/ida/modules/base_module.py
+++ brick/ida/modules/base_module.py
@@ -9,6 +9,8 @@ class BaseModule(ABC):
         self.logger = logging.getLogger('brick')
         self.res = True
         self.is_interactive = is_interactive
+        self.input_file = idaapi.get_input_file_path()
+        self.is_64bit = idaapi.get_inf_structure().is_64bit()
 
     @staticmethod
     def take_snapshot(desc):
