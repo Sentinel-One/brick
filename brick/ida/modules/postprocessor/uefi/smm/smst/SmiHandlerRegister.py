1dbb9def815d3d3c8ed556cd69ff7a36791a2d6a
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-09-18 22:49:24 +0300

    With the new efiXplorer there is no more need to process calls to SmiHandlerRegister.

--- brick/ida/modules/postprocessor/uefi/smm/smst/SmiHandlerRegister.py
+++ brick/ida/modules/postprocessor/uefi/smm/smst/SmiHandlerRegister.py
@@ -30,12 +30,14 @@ class SmiHandlerRegisterCall(CNodeExprIndirectCall):
         return self.get_arg(2)
 
     def process(self):
-        handler = self.Handler.ignore_cast
-        if not isinstance(handler, CNodeExprObj):
-            # unexpected
-            return
+        pass
+    
+        # handler = self.Handler.ignore_cast
+        # if not isinstance(handler, CNodeExprObj):
+        #     # unexpected
+        #     return
 
-        ea = hex(handler.value)[2:]
-        BipElt(handler.value).name = f'{self.COMM_BUFFER_SMI_PREFIX}_{ea}'
+        # ea = hex(handler.value)[2:]
+        # BipElt(handler.value).name = f'{self.COMM_BUFFER_SMI_PREFIX}_{ea}'
 
-        self.EFI_SMM_HANDLER_ENTRY_POINT2.set_at(handler.value)
+        # self.EFI_SMM_HANDLER_ENTRY_POINT2.set_at(handler.value)
