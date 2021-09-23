6c7451bd03edfdba017ae23b90e45494e9a7b296
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-09-23 18:38:44 +0300

    Legacy protocols module: trace unrecognized protocols as well for debugging purposes.

--- brick/ida/modules/legacy_protocols/legacy_protocols.py
+++ brick/ida/modules/legacy_protocols/legacy_protocols.py
@@ -3,8 +3,10 @@ from guids.guids_db import GuidsDatabase
 
 from bip.base import *
 
-from ...utils import brick_utils
+from ...utils import brick_utils, bip_utils
 from ..base_module import BaseModule
+
+from uuid import UUID
 
 class LegacyProtocolsModule(BaseModule):
 
@@ -15,7 +17,7 @@ class LegacyProtocolsModule(BaseModule):
         guids_db = GuidsDatabase()
         
         for guid, name in guids_db.legacy_guids.items():
-            found = brick_utils.search_guid(guid)
+            found = bip_utils.search_guid(guid)
             if found:
                 # Report that a legacy GUID was found.
                 self.res = False
@@ -23,3 +25,7 @@ class LegacyProtocolsModule(BaseModule):
 
         if self.res:
             self.logger.success('No legacy protocols were found')
+
+        for prop in BipElt.get_by_prefix('ProprietaryProtocol'):
+            data = BipData.get_bytes(prop.ea, prop.size)
+            self.logger.debug(f'Found proprietary protocol GUID {UUID(bytes_le=data)}')
