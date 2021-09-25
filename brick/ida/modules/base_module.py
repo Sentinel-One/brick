51ec45f4000f94a927f378febdd502df75ed2fe1
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-09-26 02:57:54 +0300

    'take_snapshot' should not be a method of base_module.

--- brick/ida/modules/base_module.py
+++ brick/ida/modules/base_module.py
@@ -12,20 +12,7 @@ class BaseModule(ABC):
         self.input_file = idaapi.get_input_file_path()
         self.is_64bit = idaapi.get_inf_structure().is_64bit()
 
-    @staticmethod
-    def take_snapshot(desc):
-        snapshot = idaapi.snapshot_t()
-        snapshot.desc = desc
-        idaapi.take_database_snapshot(snapshot)
-
-    def before(self):
-        # Take a snapshot before introducing any changes to the database.
-        self.take_snapshot(f'Before {self.__class__.__name__}')
-
     @abstractmethod
     def run(self):
         # Should be overriden by subclasses.
         pass
-
-    def after(self):
-        self.take_snapshot(f'After {self.__class__.__name__}')
