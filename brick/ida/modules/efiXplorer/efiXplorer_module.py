456f39d074759b10460cffb4fb1779c1466e5abf
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-09-27 01:28:14 +0300

    Add heuristic to match FreePool.

--- brick/ida/modules/efiXplorer/efiXplorer_module.py
+++ brick/ida/modules/efiXplorer/efiXplorer_module.py
@@ -44,9 +44,41 @@ class EfiXplorerModule(BaseModule):
         Initializes a database of functions that are known for generating false positives in efiXplorer.
         '''
 
+        def FreePool_heuristic(f: BipFunction):
+            # The prototype of the function must match EFI_STATUS (void *).
+            if not (f.type.nb_args == 1 and \
+                    isinstance(f.type.get_arg_type(0), BTypePtr) and \
+                    isinstance(f.type.return_type, (BTypeInt))):
+                return False
+
+            # Collect all the calls used to free pool memory.
+            def is_smm_free_pool(node: CNodeExprCall):
+                return '->SmmFreePool' in node.cstr
+            def is_bs_free_pool(node: CNodeExprCall):
+                return '->FreePool' in node.cstr
+
+            smm_free_pool = bip_utils.collect_cnode_filterlist(f.hxcfunc, is_smm_free_pool, [CNodeExprCall])
+            bs_free_pool = bip_utils.collect_cnode_filterlist(f.hxcfunc, is_bs_free_pool, [CNodeExprCall])
+
+            # Each of these functions should be called EXACLY once.
+            if len(smm_free_pool) != 1 or len(bs_free_pool) != 1:
+                return False
+
+            # The same pointer should be forwarded to both functions.
+            if smm_free_pool[0].args[0].ignore_cast.lvar != f.hxcfunc.args[0] or \
+               bs_free_pool[0].args[0].ignore_cast.lvar != f.hxcfunc.args[0]:
+                return False
+            
+            # All tests passed.
+            return True
+
         # See https://github.com/tianocore/edk2/blob/master/MdePkg/Library/SmmMemoryAllocationLib/MemoryAllocationLib.c
         FreePool_matcher = FunctionMatcher('FreePool', is_library=True)
-        if FreePool_func := FreePool_matcher.match_by_diaphora(self.res_dir / 'SmmMemoryAllocationLib.sqlite', 0.8):
+
+        if FreePool_func := FreePool_matcher.match_by_heuristic(FreePool_heuristic, decompiler_required=True):
+            self.known_false_positives.add(FreePool_func.ea)
+
+        elif FreePool_func := FreePool_matcher.match_by_diaphora(self.res_dir / 'SmmMemoryAllocationLib.sqlite', 0.8):
             self.known_false_positives.add(FreePool_func.ea)
 
     def handle_smm_callouts(self, callouts):
