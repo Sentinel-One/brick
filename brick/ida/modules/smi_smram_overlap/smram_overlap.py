fa17c2d029b366949c6cee45c5ce4a81284dbb37
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2021-10-20 16:50:19 +0300

    Refactor the SMRAM_overlap module.

--- brick/ida/modules/smi_smram_overlap/smram_overlap.py
+++ brick/ida/modules/smi_smram_overlap/smram_overlap.py
@@ -5,7 +5,6 @@ from bip.hexrays import *
 
 
 from ..smi_nested_pointers.smi import CommBufferSmiHandler
-from ...utils import bip_utils
 
 class SmramOverlapModule(BaseModule):
     '''
@@ -18,20 +17,63 @@ class SmramOverlapModule(BaseModule):
         super().__init__()
 
     @staticmethod
-    def _checks_comm_buff_size(handler: BipFunction):
+    def derefs_CommBufferSize(handler: CommBufferSmiHandler):
 
-        def deref_comm_buffer_size(node):
-            # Does the node correspond to a dereference operation on CommBufferSize? (i.e. *CommBufferSize)
-            deref = node.ops[0].ignore_cast
-            return isinstance(deref, CNodeExprVar) and (deref.lvar_name == 'CommBufferSize')
+        def _derefs_CommBufferSize(node: CNodeExprPtr):
+            '''Does the node correspond to a dereference operation on CommBufferSize? (i.e. *CommBufferSize)'''
+            dereferenced = node.ops[0].ignore_cast
 
-        return bip_utils.collect_cnode_filterlist(handler.hxcfunc, deref_comm_buffer_size, [CNodeExprPtr])
+            if isinstance(dereferenced, CNodeExprVar) and (dereferenced.lvar == handler.CommBufferSize):
+                # Found our target node, so interrupt the search.
+                return False
+
+        # If the search was interrupted, it means we found a node that dereferences CommBufferSize.
+        interrupted = not handler.hxcfunc.visit_cnode_filterlist(_derefs_CommBufferSize, [CNodeExprPtr])
+        return interrupted
+
+    @staticmethod
+    def validates_CommBuffer_not_in_smram(handler: CommBufferSmiHandler):
+
+        def _validates_CommBuffer_not_in_smram(node: CNodeExprCall):
+
+            # Check that the call is to SmmIsBufferOutsideSmmValid() from EDK2 or ValidateMemoryBuffer() from AMI.
+            if 'SmmIsBufferOutsideSmmValid' in node.cstr or '->ValidateMemoryBuffer' in node.cstr:
+                # Now check if the CommBuffer is being validated.
+                buffer = node.args[0].ignore_cast
+                if isinstance(buffer, CNodeExprVar) and (buffer.lvar == handler.CommBuffer):
+                    # Found our target node, so interrupt the search.
+                    return False
+
+        # If the search was interrupted, it means we found a node that validates CommBuffer does not overlap SMRAM.
+        interrupted = not handler.hxcfunc.visit_cnode_filterlist(_validates_CommBuffer_not_in_smram, [CNodeExprCall])
+        return interrupted
 
     def run(self):
         for handler in CommBufferSmiHandler.iter_all():
-            if not self._checks_comm_buff_size(handler):
+
+            if not handler.CommBuffer._lvar.used:
+                # If the CommBuffer is not referenced at all, the handler is assumed to be safe.
+                continue
+
+            if not handler.CommBufferSize._lvar.used:
+                # If CommBufferSize is not referenced at all, the handler is potentially vulnerable.
                 self.res = False
-                self.logger.error(f'SMI {handler.name} does not check the size of the comm buffer, check for potential overlap with SMRAM')
+                self.logger.error(f'SMI {handler.name} does not reference CommBufferSize, check for potential overlap with SMRAM')
+                continue
+
+            # If we got here, it means CommBufferSize is referenced. Still, to mark the handler as safe CommBufferSize must either either be:
+            # 1. Dereferenced to retrieve the actual size (*CommBufferSize)
+            # 2. Passed to a buffer validation routine such as SmmIsBufferOutsideSmmValid.
+
+            if self.derefs_CommBufferSize(handler):
+                # The handler checks that the CommBuffer does not overlap SMRAM simply by dereferencing the
+                # CommBufferSize argument.
+                continue
+            
+            if self.validates_CommBuffer_not_in_smram(handler):
+                # The handler checks that the CommBuffer does not overlap SMRAM by passing it to a validation routine
+                # such as SmmIsBufferOutsideSmmValid.
+                continue
 
         if self.res:
             self.logger.success('No SMI that omits checking CommBufferSize was found')
