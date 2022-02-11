63bb627a943beaa64a06d3ea09f0f1a169480f5c
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2022-02-12 01:03:25 +0200

    Add support for analyzing only specific files with idahunt.

--- brick/hunter.py
+++ brick/hunter.py
@@ -8,13 +8,21 @@ class Hunter:
 
     IDAHUNT_DIR = get_external('idahunt')
     
-    def __init__(self, dirname, arch, ext, verbose=False) -> None:
+    def __init__(self, dirname, arch=None, ext=None, names=None, verbose=False) -> None:
         self.dirname = dirname
-        self.arch = arch
-        self.ext = ext
-        self.verbose = verbose
         self.max_ida = multiprocessing.cpu_count()
         self.version = 7.7
+        
+        self.arch = arch
+        self.arch_filter = f'-a {arch}' if arch else ''
+        
+        self.ext = ext
+        self.ext_filter = f'-e {ext}' if ext else ''
+        
+        self.names = names
+        self.names_filter = f'-n {names}' if names else ''
+
+        self.verbose = verbose
 
     def _run_idahunt(self, user_args):
         idahunt_main = str(self.IDAHUNT_DIR / 'idahunt.py')
@@ -29,7 +37,7 @@ class Hunter:
         return subprocess.check_call(args)
 
     def analyze(self):
-        return self._run_idahunt(['--filter', fr"filters\names.py -a {self.arch} -e {self.ext} -v", '--analyse'])
+        return self._run_idahunt(['--filter', fr"filters\names.py {self.arch_filter} {self.ext_filter} {self.names_filter} -v", '--analyse'])
 
     def cleanup(self):
         return self._run_idahunt(['--cleanup'])
@@ -41,5 +49,5 @@ class Hunter:
             script = str(resolved)
 
         script_with_args = f'{script} ' + ' '.join(args)
-        return self._run_idahunt(['--filter', fr"filters\names.py -a {self.arch} -e {self.ext} -v", '--scripts', script_with_args])
+        return self._run_idahunt(['--filter', fr"filters\names.py {self.arch_filter} {self.ext_filter} {self.names_filter} -v", '--scripts', script_with_args])
         
