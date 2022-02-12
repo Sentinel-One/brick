2b56c0b574bfb996fc010b12c6967cd37622d2fc
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2022-02-12 04:00:06 +0200

    Add binaries for tests + tests for SMM callouts.

---
+++ brick/tests/test_smm_callouts.py
@@ -0,0 +1,22 @@
+import pytest
+import sys
+sys.path.append('..')
+
+from brick_api import scan_file
+
+def text_in_file(text, filename):
+    data = open(filename, 'r').read()
+    return text in data
+
+def test_FreePool_false_callout():
+    scan_file('bin/000C.efi', 'callouts')
+    assert text_in_file('SUCCESS (callouts.py)', 'bin/000C.brick')
+
+def test_SmmRuntimeService_false_callout():
+    scan_file('bin/SmiFlash.efi', 'callouts')
+    assert text_in_file('SUCCESS (callouts.py)', 'bin/SmiFlash.brick')
+
+def test_true_callout():
+    scan_file('bin/0511.efi', 'callouts')
+    assert text_in_file('ERROR (callouts.py)', 'bin/0511.brick')
+    
