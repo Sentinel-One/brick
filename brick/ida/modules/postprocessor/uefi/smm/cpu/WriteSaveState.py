0ee7450eab3f0e07ec5e0371de04deee95609420
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2022-02-25 15:29:28 +0200

    Add parameters to WriteSaveState.

--- brick/ida/modules/postprocessor/uefi/smm/cpu/WriteSaveState.py
+++ brick/ida/modules/postprocessor/uefi/smm/cpu/WriteSaveState.py
@@ -4,3 +4,26 @@ class WriteSaveStateCall(CNodeExprIndirectCall):
     """Represents a call to WriteSaveState()"""
     
     PROTOTYPE = 'EFI_SMM_WRITE_SAVE_STATE'
+
+    @property
+    def This(self):
+        return self.get_arg(0)
+
+    @property
+    def Width(self):
+        return self.get_arg(1)
+
+    @property
+    def Register(self):
+        return self.get_arg(2)
+
+    @property
+    def CpuIndex(self):
+        return self.get_arg(3)
+
+    @property
+    def Buffer(self):
+        return self.get_arg(4)
+
+    def process(self):
+        pass
