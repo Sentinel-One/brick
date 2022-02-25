edec233bf666ed8c35b05474273b2f4f8bb64e61
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2022-02-25 19:08:48 +0200

    Rename test file.

--- brick/tests/test_callouts.py
+++ brick/tests/test_callouts.py
@@ -8,16 +8,15 @@ def text_in_file(text, filename):
     data = open(filename, 'r').read()
     return text in data
 
-def test_FreePool_false_callout(session_dir):
-    print('Session dir ', session_dir)
+def test_FreePool(session_dir):
     scan_file(f'{session_dir}/000C.efi', 'callouts')
     assert text_in_file('SUCCESS (callouts.py)', f'{session_dir}/000C.brick')
 
-def test_SmmRuntimeService_false_callout(session_dir):
+def test_SmmRuntimeService(session_dir):
     scan_file(f'{session_dir}/SmiFlash.efi', 'callouts')
     assert text_in_file('SUCCESS (callouts.py)', f'{session_dir}/SmiFlash.brick')
 
-def test_true_callout(session_dir):
+def test_BootServices_callout(session_dir):
     scan_file(f'{session_dir}/0511.efi', 'callouts')
     assert text_in_file('ERROR (callouts.py)', f'{session_dir}/0511.brick')
     
