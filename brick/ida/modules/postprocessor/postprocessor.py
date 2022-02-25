e652edb445d87965813794e0cccfd6e6a45a35b9
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2022-02-25 15:28:57 +0200

    Clean-up the postprocessor a bit.

--- brick/ida/modules/postprocessor/postprocessor.py
+++ brick/ida/modules/postprocessor/postprocessor.py
@@ -1,5 +1,6 @@
 from bip.base import *
 from bip.hexrays import *
+from brick.ida.modules.efiXplorer.efiXplorer import EfiXplorerModule, EfiXplorerPlugin
 
 from ..base_module import BaseModule
 
@@ -13,15 +14,17 @@ from ...utils import bip_utils
 
 class PostprocessorModule(BaseModule):
 
-    EFI_SMM_HANDLER_ENTRY_POINT = BipType.from_c("""EFI_STATUS (f)(
-        EFI_HANDLE DispatchHandle,
-        void * Context,
-        EFI_SMM_SW_CONTEXT * CommBuffer,
-        UINTN * CommBufferSize)""")
+    DEPENDS_ON = [EfiXplorerModule]
 
     def _fix_sw_smis_signatures(self):
-        for sw_smi_handler in BipFunction.get_by_prefix("SwSmiHandler"):
-            self.EFI_SMM_HANDLER_ENTRY_POINT.set_at(sw_smi_handler.ea)
+        EFI_SMM_HANDLER_ENTRY_POINT = BipType.from_c("""EFI_STATUS (f)(
+            EFI_HANDLE DispatchHandle,
+            void * Context,
+            EFI_SMM_SW_CONTEXT * CommBuffer,
+            UINTN * CommBufferSize)""")
+
+        for sw_smi_handler in BipFunction.get_by_prefix(EfiXplorerPlugin.SW_SMI_PREFIX):
+            EFI_SMM_HANDLER_ENTRY_POINT.set_at(sw_smi_handler.ea)
 
     def _fix_efi_services_signatures(self):
 
@@ -33,31 +36,36 @@ class PostprocessorModule(BaseModule):
 
     def _rename_enums(self):
         idc.import_type(-1, 'MACRO_EFI')
-        me = BipEnum.get('MACRO_EFI')
+        macro_efi = BipEnum.get('MACRO_EFI')
 
         for ins in BipInstr.iter_all():
             for (op_id, op) in enumerate(ins.ops):
                 if op.type == BipOpType.IMM and op.value & 0x8000000000000000:
-                    idc.op_enum(ins.ea, op_id, me._eid, 0)
+                    idc.op_enum(ins.ea, op_id, macro_efi._eid, 0)
 
     def _handle_assignments(self):
-
+        '''
+        Handles assignment of one global object to another, propagating the types if necessary.
+        '''
         def callback(node: CNode):
             assert isinstance(node, CNodeExprAsg)
 
+            # Check that both objects are globals
             src = node.src.ignore_cast
             dst = node.dst.ignore_cast
 
-            # Check that both objects are globals
-            if isinstance(src, CNodeExprObj) and isinstance(dst, CNodeExprObj):
-                # The destination operand is merely an integer.
-                if isinstance(dst.type, BTypeInt):
-                    # The source is a pointer to a structure.
-                    if isinstance(src.type, BTypePtr) and isinstance(src.type.pointed, BTypeStruct):
-                        # Copy type.
-                        src.type.set_at(dst.value)
-                        node.hxcfunc.invalidate_cache()
-                        return True
+            if not isinstance(src, CNodeExprObj) or not isinstance(dst, CNodeExprObj):
+                return
+
+            # The destination operand is merely an integer.
+            if isinstance(dst.type, BTypeInt):
+                # The source is a pointer to a structure.
+                if isinstance(src.type, BTypePtr) and isinstance(src.type.pointed, BTypeStruct):
+                    # Copy type.
+                    src.type.set_at(dst.value)
+                    self.logger.debug(f'Assigning type {src.type.str} to 0x{dst.value:x}')
+                    node.hxcfunc.invalidate_cache()
+                    return True
 
         modified = bip_utils.collect_cnode_filterlist(None, callback, [CNodeExprAsg])
         while modified:
