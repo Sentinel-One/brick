b6da86b396d84fc0716294b24ebd20c702b8f806
Assaf Carlsbad <assaf.carlsbad@gmail.com>
2022-04-28 13:20:14 +0300

    Post-process calls to SmiHandlerUnRegister.

--- brick/ida/modules/postprocessor/uefi/smm/smst/SmiHandlerRegister.py
+++ brick/ida/modules/postprocessor/uefi/smm/smst/SmiHandlerRegister.py
@@ -45,3 +45,15 @@ class SmiHandlerRegisterCall(CNodeExprIndirectCall):
 
         logging.getLogger('brick').info(f'Discovered an SMI handler at 0x{handler.value:x}')
         
+class SmiHandlerUnRegisterCall(CNodeExprIndirectCall):
+    """Represents a call to SmiHandlerUnRegister()"""
+
+    PROTOTYPE = 'EFI_SMM_INTERRUPT_UNREGISTER'
+
+    @property
+    def DispatchHandle(self):
+        return self.get_arg(0)
+
+    def process(self):
+        pass
+    
--- brick/ida/modules/postprocessor/uefi/smm/smst/__init__.py
+++ brick/ida/modules/postprocessor/uefi/smm/smst/__init__.py
@@ -1,8 +1,9 @@
 from ...bs.LocateProtocol import LocateProtocolCall
 from ...factory.factory import UefiCallFactory
 
-from .SmiHandlerRegister import SmiHandlerRegisterCall
+from .SmiHandlerRegister import SmiHandlerRegisterCall, SmiHandlerUnRegisterCall
 
 SmstFactory = UefiCallFactory()
 SmstFactory.register('SmiHandlerRegister',  SmiHandlerRegisterCall)
+SmstFactory.register('SmiHandlerUnRegister',  SmiHandlerUnRegisterCall)
 SmstFactory.register('SmmLocateProtocol',   LocateProtocolCall)
